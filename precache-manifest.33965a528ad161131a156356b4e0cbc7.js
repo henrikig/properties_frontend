49fcd8cf84477a63c4807a0cb6db58a53354c266
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2020-05-06 21:00:55 +0200

    Updates

---
+++ precache-manifest.33965a528ad161131a156356b4e0cbc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51705db11c16e3d8db6601602f77e71",
+    "url": "/properties_frontend/index.html"
+  },
+  {
+    "revision": "e82e5a3ed3558d02a979",
+    "url": "/properties_frontend/static/css/main.970941ba.chunk.css"
+  },
+  {
+    "revision": "97c2a1315efc95e7e1e1",
+    "url": "/properties_frontend/static/js/2.350fc164.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/properties_frontend/static/js/2.350fc164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82e5a3ed3558d02a979",
+    "url": "/properties_frontend/static/js/main.826d20b8.chunk.js"
+  },
+  {
+    "revision": "73aecb6dfb36dff2f55e",
+    "url": "/properties_frontend/static/js/runtime-main.4e234341.js"
+  }
+]);
